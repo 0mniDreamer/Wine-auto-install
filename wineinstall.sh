19ec66f3f1e9d8aa17ca3cae7dd75e236fd42f76
OmniDreamer <75704897+0mniDreamer@users.noreply.github.com>
2023-02-21 00:04:54 +0000

    Updated to include comments and install wine tricks Update wineinstall.sh

--- wineinstall.sh
+++ wineinstall.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo "Welcome to the Wine auto installer"
+echo "Welcome to the Wine Auto Installer"
 
+# Enable the 32-bit archicture support for Wine 
 sudo dpkg --add-architecture i386
 
-sudo mkdir -pm755 /etc/apt/keyrings
+# Make Directory to hold the Wine-HQ key and retreieve the key 
+sudo mkdir -pm755 /etc/apt/keyrings/
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+
+# Update the sources list with the WineHQ repo.
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources # Ubuntu 22.04 OS repo 
+
+# Update the local repo list file to populate the new repos.
 sudo apt update
-sudo apt install --install-recommends winehq-staging
+
+# Install Wine Staging from the WineHQ repo
+sudo apt install -y --install-recommends winehq-staging
+
+# Install the winetrick package
+sudo apt install -y winetricks 
+
+# Enable the winetricks self updater
+sudo winetricks --self-update 
